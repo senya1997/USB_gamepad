725fe44d72fc6e74e14ef3cdd59d822309fa5a5c
senya_1997 <senya_1997@mail.ru>
2019-10-30 10:39:58 +0300

    test firmware flashing by bootloader

--- gamepad/gamepad/defines.h
+++ gamepad/gamepad/defines.h
@@ -1,3 +1,25 @@
 #define F_CPU 16000000L
 
-// IO ports:
+// SEGA:
+#define PORT_SEGA PORTB
+#define DDR_SEGA DDRB
+
+// 0..5 - PORTB, 7 - PORTD
+#define SEGA_UP_Z		0
+#define SEGA_DOWN_Y		1
+#define SEGA_LEFT_X		2
+#define SEGA_RIGHT_MODE	3
+#define SEGA_A_B		4
+#define SEGA_ST_C		5 /* "start" or "C" */
+#define SEGA_SEL		7 /* select */
+
+// PS:
+#define PORT_PS PORTC
+#define DDR_PS DDRC
+
+// LED:
+#define PORT_LED PORTD
+#define DDR_LED DDRD
+
+#define LED0 5
+#define LED1 6
--- gamepad/gamepad/main.c
+++ gamepad/gamepad/main.c
@@ -1,15 +1,39 @@
 #include "defines.h"
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 
 #include "usbdrv/usbdrv.h"
 #include "descriptors.h"
 
+USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
+{
+	return 1;
+}
+
+void hardwareInit(void)
+{
+	DDR_LED = (1 << LED0) | (1 << LED1); 
+}
+
 void main(void)
 {
+	
+	hardwareInit();
+	usbDeviceConnect();
+	usbInit();
+	
+	asm("SEI");
     while (1) 
     {
+		usbPoll();
 		
+		_delay_ms(300);
+		PORT_LED ^= (1 << LED0);
+		
+		_delay_ms(300);
+		PORT_LED ^= (1 << LED1);
     }
 }
 
