27a1b124076e34a187f76e783f39f64e33052fb6
SS <senya_1997@mail.ru>
2022-08-28 21:25:18 +0300

    TEMP

--- gamepad/gamepad/main.c
+++ gamepad/gamepad/main.c
@@ -7,34 +7,21 @@
 #include "usbdrv/usbdrv.h"
 #include "descriptor.h"
 
-uchar report_buf[REPORT_SIZE] = {0x7F, 0x7F, 0x7F, 0x7F, 0x00, 0x00}; // OX, OY, RX, RY, but 1st plr, but 2nd plr
-	
+#ifdef DEBUG
+	#warning "DEBUG is enabled"
+#endif
+
+#ifdef PROTEUS
+	#warning "PROTEUS SIM is enabled"
+#endif
+
+uchar report_buf[REPORT_SIZE] = {0x7F, 0x7F, 0x7F, 0x7F, 0x00, 0x00};
+
 uchar delay_idle = INIT_IDLE_TIME; // step - 4ms
 uchar cnt_idle = 0;
-
-uchar flag_report = 0;
 uchar flag_idle = 0; // shows that idle time is over and can send report
 
-uchar flag_ps = 0;
-uchar flag_first_run = 1;	// to enable PS controllers, hold START on SEGA 1st controller before and after connect device to PC on 2-3 sec
-							// otherwise by default work SEGA controller mode
-
-// SEGA var and protocol:
-	uchar flag_ch_gp = 1; // shows that required save buttons state
-	uchar state = 0; // 0..7 states
-
-/*  _____________________________
-	|Sel |D0 |D1 |D2 |D3 |D4 |D5 |
-	+----+---+---+---+---+---+---+
-	| L  |UP |DW |LO |LO |A  |ST |
-	| H  |UP |DW |LF |RG |B  |C  |
-	| L  |UP |DW |LO |LO |A  |ST |
-	| H  |UP |DW |LF |RG |B  |C  |
-	| L  |LO |LO |LO |LO |A  |ST |
-	| H  |Z  |Y  |X  |MD |HI |HI |
-	| L  |HI |HI |HI |HI |A  |ST |
-	| H  |UP |DW |LF |RG |B  |C  |
-*/
+uchar flag_report = 0; // shows that information from gamepad is ready to form like in descriptor
 
 // PS var and protocol:
 	uchar shift_report_buf[REPORT_SIZE];
@@ -42,9 +29,9 @@ uchar flag_first_run = 1;	// to enable PS controllers, hold START on SEGA 1st co
 	uchar cnt_byte = 0;
 	uchar cnt_edge = 0;
 	uchar cnt_rep_buf = 5;
-
+	
 	uchar offset;
-
+	
 /*********************************************************************************/
 /* CLK ~ 7 kHz, issue data LSB on MISO and MOSI on falling edge, read on front   */
 /* seq from MC:  0x01 | 0x42 | 0xFF | 0xFF | 0xFF | 0xFF | 0xFF | 0xFF | 0xFF    */
@@ -101,44 +88,155 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8])
 					return 1;
 				}
 				break;
-			//case USBRQ_HID_SET_IDLE: return USB_NO_MSG; // call "usbFunctionWrite" ("OUT" token)
+			case USBRQ_HID_SET_IDLE: return USB_NO_MSG; // call "usbFunctionWrite" ("OUT" token)
+			/*
 			case USBRQ_HID_SET_IDLE:
 				// mb required reset "cnt_idle" and enable interrupt timer 0, cause 
 				// "flag_idle" can be set during "delay_idle" change in large way
 				if(rq -> wValue.bytes[1] != 0) delay_idle = rq -> wValue.bytes[1];
-				else delay_idle = 3; // when the upper byte of "wValue" = 0, the duration is indefinite
+				else delay_idle = INIT_IDLE_TIME; // when the upper byte of "wValue" = 0, the duration is indefinite
+				*/
 		}
 	}
 	
 	return 0; // ignore data from host ("OUT" token)
 }
 
-uchar *upd_SEGA_ReportBuf(uchar offset, uchar *gp_state_ptr) // offset defines by player number: 1st - "0", 2nd - "8"
+USB_PUBLIC uchar usbFunctionWrite(uchar *data, uchar len)
 {
-	static uchar int_report_buf[3]; // internal report buf: on exit of function - OX[0], OY[1], buttons[2]
-	uchar temp;
+	usbRequest_t *rq = (usbRequest_t*)data;
 	
-			// 2,3,5 - SEL number at which data were polling in protocol (see "state" comment)
-	temp = (~(*(gp_state_ptr + 2 + offset))) & ST_A_MASK;	// 0b00110000
-		int_report_buf[2] = temp << 2;
-
-	temp = (~(*(gp_state_ptr + 3 + offset))) & C_B_MASK;	// 0b00110000
-		int_report_buf[2] |= temp;
-
-	temp = (~(*(gp_state_ptr + 5 + offset))) & ZYX_MD_MASK;	// 0b00001111
-		int_report_buf[2] |= temp;
-
-	temp = (~(*(gp_state_ptr + 3 + offset))) & LF_RG_MASK;	// 0b00000011
-		if(temp == UP_MASK) int_report_buf[1] = 0x00;
-		else if(temp == DW_MASK) int_report_buf[1] = 0xFF;
-		else int_report_buf[1] = 0x7F;
-
-	temp = (~(*(gp_state_ptr + 3 + offset))) & UP_DW_MASK;	// 0b00001100
-		if(temp == RG_MASK) int_report_buf[0] = 0xFF;
-		else if(temp == LF_MASK) int_report_buf[0] = 0x00;
-		else int_report_buf[0] = 0x7F;
+	if(rq -> wValue.bytes[1] != 0) delay_idle = rq -> wValue.bytes[1];
+	else delay_idle = INIT_IDLE_TIME; // when the upper byte of "wValue" = 0, the duration is indefinite
 	
-	return int_report_buf; // return pointer on massive
+	return 1;
+}
+
+inline void hardwareInit()
+{
+	DDR_LED = (1 << LED0) | (1 << LED1);
+
+	// for idle time:
+	TCCR1B = (1 << CS10);
+	TIMSK1 = (1 << TOIE1);
+	
+// this func call after "hardware_SEGA_init" when PS mode is activating
+	DDR_PS &= ~(1 << PS_MISO) & ~(1 << PS_ACK); // inputs
+#ifdef DEBUG
+	DDR_PS |= (1 << PS_CS) | (1 << PS_MOSI) | (1 << PS_CLK) | (1 << PS_DEBUG); // outputs
+#else 
+	DDR_PS |= (1 << PS_CS) | (1 << PS_MOSI) | (1 << PS_CLK);
+#endif
+	
+// add pullup on inputs and issue one on outputs:
+// 				***** ATTENTION *****
+// on MISO PULLUP external and must be turn off on mc
+	PORT_PS &= ~(1 << PS_ACK) & ~(1 << PS_MISO); // no pullup
+	PORT_PS = (1 << PS_CS) | (1 << PS_CLK);
+	
+// duplicate main timer:
+	TCCR0A = (1 << WGM01);
+	TCCR0B = (1 << CS01); // presc = 8 => 70 us <=> 140 cnt
+	TIMSK0 = (1 << OCIE0A);
+	OCR0A = CLK_HALF_PER + DELTA;
+	
+// for PS CLK ~ 7 kHz, DO NOT forget to approve with CPU freq:
+	TCCR2A = (1 << WGM21); // CTC mode with OCR2A 
+	TCCR2B = (1 << CS21); // presc = 8 => half period CLK 70 us <=> 140 cnt
+	OCR2A = CLK_HALF_PER;
+	TIMSK2 = (1 << OCIE2A);
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	TIMSK0 &= ~(1 << OCIE0A);
+	TIMSK2 &= ~(1 << OCIE2A);
+	
+		sei();
+		
+		if(cnt_idle < delay_idle) cnt_idle++;
+		else
+		{
+			TIMSK1 &= ~(1 << TOIE1);
+			flag_idle = 1;
+		}
+	
+	TIMSK0 |= (1 << OCIE0A);
+	TIMSK2 |= (1 << OCIE2A);
+}
+
+#define END_ONE_BYTE (cnt_edge == 18)
+#define ACT_TRANS (cnt_edge < 16)
+#define IDLE_STATE (cnt_edge == 15)
+#define LAST_BYTE (cnt_byte == 9)
+
+ISR(TIMER2_COMPA_vect)
+{	
+	TCNT0 = 5; // reset duplicate timer
+	TIMSK0 &= ~(1 << OCIE0A); // "cli"
+	
+	sei();
+	
+	if((TIFR0 & (1 << OCF0A)) == (1 << OCF0A)) clearShiftBuf();
+	
+	// CLK:
+	if((cnt_byte > 0) & ACT_TRANS) PORT_PS ^= (1 << PS_CLK);
+	
+	// MISO:
+	if((cnt_byte > 3) & ACT_TRANS & ((cnt_edge & 0x01) == 1)) // odd "cnt_edge"
+	{
+		offset = (cnt_edge - 1) >> 1;
+		shift_report_buf[cnt_rep_buf] |= (PIN_PS & 0x01) << offset;
+	}
+	
+	// MOSI:
+	if(((cnt_byte == 1) & (cnt_edge < 2)) |
+	  ((cnt_byte == 2) & ((cnt_edge == 2) | (cnt_edge == 3) | (cnt_edge == 12) | (cnt_edge == 13)))) PORT_PS |= (1 << PS_MOSI);
+	else PORT_PS &= ~(1 << PS_MOSI);
+	
+	// CS:
+	if((cnt_byte == 0) & IDLE_STATE) PORT_PS &= ~(1 << PS_CS);
+	else if(LAST_BYTE & END_ONE_BYTE) PORT_PS |= (1 << PS_CS);
+	
+	// counters - required go to ASM:
+	if(cnt_byte == 0) // between byte transmit
+	{
+		if(IDLE_STATE)
+		{
+			cnt_edge = 0;
+			cnt_byte++;
+		}
+		else cnt_edge++;
+	}
+	else if((cnt_byte > 0) & (cnt_byte < 4)) 
+	{
+		if(END_ONE_BYTE)
+		{
+			cnt_edge = 0;
+			cnt_byte++;
+		}
+		else cnt_edge++;
+	}
+	else
+	{
+		if(END_ONE_BYTE)
+		{
+			cnt_edge = 0;
+			cnt_rep_buf--;
+			
+			if(LAST_BYTE)
+			{
+				flag_report = 1;
+				cnt_byte = 0;
+				
+				//TIMSK1 &= ~(1 << OCIE1A);
+			}
+			else cnt_byte++;
+		}
+		else cnt_edge++;
+	}
+	
+	TIMSK0 |= (1 << OCIE0A); // "sei"
 }
 
 inline void clearShiftBuf()
@@ -150,7 +248,7 @@ inline void clearShiftBuf()
 		shift_report_buf[2] = 0;
 		shift_report_buf[3] = 0;
 		shift_report_buf[4] = 0;
-		shift_report_buf[5] = 0;
+		shift_report_buf[5] = 0;	
 	}
 	
 	cnt_rep_buf = 5;
@@ -159,198 +257,33 @@ inline void clearShiftBuf()
 	cnt_byte = 0;
 	
 	PORT_PS |= (1 << PS_CS) | (1 << PS_CLK);
-	TIFR1 |= (1 << OCF1A);
+	TIFR0 |= (1 << OCF0A);
 }
 
-void hardware_SEGA_Init()
-{
-	DDR_LED = (1 << LED0) | (1 << LED1);
-		
-	// gamepads:
-		DDR_SEGA_AUX = (1 << SEGA_SEL);
-		PORT_SEGA_AUX &= ~(1 << SEGA_SEL); // necessarily down to zero SEL signal on start
-		
-		PORT_SEGA1 = (1 << SEGA_LF_X) | (1 << SEGA_RG_MD) | (1 << SEGA_UP_Z) |
-					 (1 << SEGA_DW_Y) | (1 << SEGA_A_B) | (1 << SEGA_ST_C);
-		PORT_SEGA2 = (1 << SEGA_LF_X) | (1 << SEGA_RG_MD) | (1 << SEGA_UP_Z) |
-					 (1 << SEGA_DW_Y) | (1 << SEGA_A_B) | (1 << SEGA_ST_C);
-
-	// timers:
-		TCCR0A = (1 << WGM01); // CTC mode with OCRA
-		TCCR0B = (1 << CS02); // presc = 256 => 4 ms <=> 250 cnt
-		OCR0A = STEP_IDLE_CONF;
-		
-		TCCR2A = (1 << WGM21); // CTC mode with OCRA
-		TCCR2B = (1 << CS20) | (1 << CS22); // presc = 128 => 2 ms <=> 250 cnt; 500 us <=> 62.5
-		OCR2A = PER_POLL_GP;
-		
-		TIMSK0 = (1 << OCIE0A);
-		TIMSK2 = (1 << OCIE2A);
-}
-
-inline void hardware_PS_Init()
-{
-	PORT_LED |= (1 << LED0);
-
-	#ifdef DEBUG
-		DDR_PS = (1 << PS_CS) | (1 << PS_MOSI) | (1 << PS_CLK) | (1 << PS_DEBUG); // outputs
-	#else
-		DDR_PS = (1 << PS_CS) | (1 << PS_MOSI) | (1 << PS_CLK);
-	#endif
-	
-	// add pullup on inputs and issue one on outputs:
-		PORT_PS = (1 << PS_CS) | (1 << PS_CLK);
-	
-	// duplicate main timer:
-		TIMSK0 &= ~(1 << OCIE0A); // disable idle timer
-		
-		TCCR1B = (1 << WGM12) | (1 << CS11); // presc = 8 => 70 us <=> 140 cnt
-		TIMSK1 = (1 << OCIE1A);
-		OCR1A = CLK_HALF_PER + DELTA;
-	
-	// for PS CLK ~ 7 kHz, DO NOT forget to approve with CPU freq:
-		TCCR2A = (1 << WGM21); // CTC mode with OCR2A
-		TCCR2B = (1 << CS21); // presc = 8 => half period CLK 70 us <=> 140 cnt
-		OCR2A = CLK_HALF_PER;
-		TIMSK2 = (1 << OCIE2A);
-}
-
-ISR(TIMER0_COMPA_vect) // SEGA idle time
-{
-	TIMSK2 &= ~(1 << OCIE2A); // "cli" for avoid nested interrupts
-		sei();
-		
-		if(cnt_idle < delay_idle) cnt_idle++;
-		else
-		{
-			TIMSK0 &= ~(1 << OCIE0A);
-			flag_idle = 1;
-		}
-	TIMSK2 |= (1 << OCIE2A); // "sei"
-}
-
-ISR(TIMER2_COMPA_vect)
-{
-	if(flag_ps) // PS:
-	{
-		TCNT1 = 5; // reset duplicate timer
-		TIMSK1 &= ~(1 << OCIE1A); // "cli"
-	
-		sei();
-	
-		if((TIFR1 & (1 << OCF1A)) == (1 << OCF1A)) clearShiftBuf();
-	
-		// CLK:
-			if((cnt_byte > 0) & ACT_TRANS) PORT_PS ^= (1 << PS_CLK);
-	
-		// MISO:
-			if((cnt_byte > 3) & ACT_TRANS & ((cnt_edge & 0x01) == 1)) // odd "cnt_edge"
-			{
-				offset = (cnt_edge - 1) >> 1;
-				shift_report_buf[cnt_rep_buf] |= (PIN_PS & 0x01) << offset;
-			}
-	
-		// MOSI:
-			if(((cnt_byte == 1) & (cnt_edge < 2)) |
-			   ((cnt_byte == 2) & ((cnt_edge == 2) | (cnt_edge == 3) | (cnt_edge == 12) | (cnt_edge == 13)))) PORT_PS |= (1 << PS_MOSI);
-			else PORT_PS &= ~(1 << PS_MOSI);
-	
-		// CS:
-			if((cnt_byte == 0) & IDLE_STATE) PORT_PS &= ~(1 << PS_CS);
-			else if(LAST_BYTE & END_ONE_BYTE) PORT_PS |= (1 << PS_CS);
-	
-		// counters - required go to ASM:
-			if(cnt_byte == 0) // between byte transmit
-			{
-				if(IDLE_STATE)
-				{
-					cnt_edge = 0;
-					cnt_byte++;
-				}
-				else cnt_edge++;
-			}
-			else if((cnt_byte > 0) & (cnt_byte < 4))
-			{
-				if(END_ONE_BYTE)
-				{
-					cnt_edge = 0;
-					cnt_byte++;
-				}
-				else cnt_edge++;
-			}
-			else
-			{
-				if(END_ONE_BYTE)
-				{
-					cnt_edge = 0;
-					cnt_rep_buf--;
-			
-					if(LAST_BYTE)
-					{
-						flag_report = 1;
-						cnt_byte = 0;
-				
-						//TIMSK1 &= ~(1 << OCIE1A);
-					}
-					else cnt_byte++;
-				}
-				else cnt_edge++;
-			}
-	
-		TIMSK1 |= (1 << OCIE1A); // "sei"
-	}
-	else // SEGA:
-	{
-		TIMSK0 &= ~(1 << OCIE0A); // "cli"
-		sei();
-		
-		if(state < 7)
-		{
-			PORT_SEGA_AUX ^= (1 << SEGA_SEL);
-			
-			flag_ch_gp = 1;
-			state++;
-		}
-		else if(state == 8)
-		{ // after delay between "packets":
-			OCR2A = PER_POLL_GP;
-			flag_ch_gp = 1;
-			state = 0;
-		}
-		else
-		{ // after "packet":
-			PORT_SEGA_AUX &= ~(1 << SEGA_SEL);
-			OCR2A = DELAY_BTW_POLL;
-			
-			flag_report = 1;
-			state = 8;
-		}
-		TIMSK0 |= (1 << OCIE0A); // "sei"
-	}
-}
-
-void main_PS()
+void main()
 {
 	char flag_report_rdy = 0;
 	
-	hardware_PS_Init();
+	hardwareInit();
 	
 	#ifndef DEBUG
 		usbDeviceConnect();
 		usbInit();
 	#endif
 	
-	// full reset timers:
-		TCNT1 = 0;
-		TCNT2 = 0;
+// full reset timers:
+	TCNT0 = 0;
+	TCNT1 = 0;
+	TCNT2 = 0;
 	
-		TIFR1 |= (1 << OCF1A);
-		TIFR2 |= (1 << OCF2A);
+	TIFR0 |= (1 << OCF0A);
+	TIFR1 |= (1 << TOV1);
+	TIFR2 |= (1 << OCF2A);
 	
 	sei();
-	while (1)
-	{
-		if(flag_report_rdy) // mb idle time not required
+    while (1) 
+    {
+		if(flag_idle & flag_report_rdy) // send report immediately after "idle" time has passed:
 		{
 			if(usbInterruptIsReady())
 			{
@@ -365,7 +298,10 @@ void main_PS()
 				//clearShiftBuf();
 				flag_report_rdy = 0;
 				
-				PORT_LED |= (1 << LED0);
+			// full reset idle timer then enable interrupt:
+				TCNT1 = 0;
+				TIFR1 |= (1 << TOV1); 
+				TIMSK1 |= (1 << TOIE1);
 			}
 		}
 		
@@ -392,104 +328,12 @@ void main_PS()
 			flag_report = 0;
 			flag_report_rdy = 1;
 		}
-
-		if((cnt_byte == 0) & (cnt_edge == 10)) usbPoll();
-	}
-}
-
-void main(void)
-{
-	#ifdef DEBUG
-		uchar gp_state_buf[2][8] = {0x00, 0x00, 0x10, 0x15, 0x00, 0x0A, 0x00, 0x00,
-									0x00, 0x00, 0x20, 0x2A, 0x00, 0x05, 0x00, 0x00};
-	#else
-		uchar gp_state_buf[2][8];
-	#endif
-	
-	uchar *report_buf_ptr;
-
-	hardware_SEGA_Init();
-	
-	// full reset timers:
-		TCNT0 = 0;
-		TCNT2 =0;
-	
-		TIFR0 |= (1 << OCF0A);
-		TIFR2 |=(1 << OCF2A); 
-	
-		GTCCR |= (1 << PSRASY); // reset presc timers
-	
-	sei();
-    while (1) 
-    {
-		#ifndef DEBUG
-			usbPoll(); // ~ 9.63 us (all timings write in 16 MHz CPU freq)
-		#endif
 		
-		if(flag_idle) // send report immediately after "idle" time has passed:
+		if((report_buf[4] != 0x00) | (report_buf[5] != 0x00)) PORT_LED ^= (1 << LED0);
+		
+		if((cnt_byte == 0) & (cnt_edge == 10))
 		{
-			if(usbInterruptIsReady())
-			{
-				#ifndef DEBUG
-					usbSetInterrupt(report_buf, REPORT_SIZE);  // ~  us
-				#endif
-				
-				#ifdef PROTEUS
-					usbSetInterrupt(report_buf, REPORT_SIZE);
-				#endif
-				
-				cnt_idle = 0;
-				flag_idle = 0;
-				
-				PORT_LED |= (1 << LED0);
-				
-			// full reset timer 0 then enable interrupt:
-				TCNT0 = 0;
-				TIFR0 |= (1 << OCF0A); 
-				TIMSK0 |= (1 << OCIE0A);
-			}
-		}
-		
-		if(flag_report) // build report:
-		{ 
-			report_buf_ptr = upd_SEGA_ReportBuf(0, (uchar *)gp_state_buf); // var that defining the array is also a pointer to it
-				report_buf[0] = *report_buf_ptr;
-				report_buf[1] = *(report_buf_ptr + 1);
-				report_buf[4] = *(report_buf_ptr + 2);
-				
-			report_buf_ptr = upd_SEGA_ReportBuf(8, (uchar *)gp_state_buf);
-				report_buf[2] = *report_buf_ptr;
-				report_buf[3] = *(report_buf_ptr + 1);
-				report_buf[5] = *(report_buf_ptr + 2);
-			
-			if(flag_first_run)
-			{
-				cli();
-				
-				flag_first_run = 0;
-				
-				if((report_buf[4] == PS_ON))
-				{
-					flag_ps = 1;
-					main_PS();
-				}
-				
-				#ifndef DEBUG
-					usbDeviceConnect();
-					usbInit();
-				#endif
-				
-				sei();
-			}
-			
-			flag_report = 0;
-		}
-		
-		if(flag_ch_gp & (TCNT2 >= DELAY_BEF_POLL)) // upd gamepad status buffer:
-		{
-			gp_state_buf[0][state] = PIN_SEGA1 & SEGA_PIN_MASK;
-			gp_state_buf[1][state] = PIN_SEGA2 & SEGA_PIN_MASK;
-			flag_ch_gp = 0;
+			 usbPoll();
 		}
     }
 }
